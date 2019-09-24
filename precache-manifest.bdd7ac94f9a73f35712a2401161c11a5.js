85242476032524a43360096aef4ac90e9b678cdf
Amanda Chau <amandachau36@gmail.com>
2019-09-24 10:58:15 +1000

    Updates

---
+++ precache-manifest.bdd7ac94f9a73f35712a2401161c11a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee730a766aea8987c4e53682963be02a",
+    "url": "/oxford-hackathon/index.html"
+  },
+  {
+    "revision": "ff03a8466b0a6263b71a",
+    "url": "/oxford-hackathon/static/css/main.69c5d212.chunk.css"
+  },
+  {
+    "revision": "21f3ab06f2972aab5402",
+    "url": "/oxford-hackathon/static/js/2.dc1811fd.chunk.js"
+  },
+  {
+    "revision": "ff03a8466b0a6263b71a",
+    "url": "/oxford-hackathon/static/js/main.085590b9.chunk.js"
+  },
+  {
+    "revision": "4c15c7f467f44fa9ce8c",
+    "url": "/oxford-hackathon/static/js/runtime~main.c92905d4.js"
+  },
+  {
+    "revision": "0cbd653de0f4f0e9f7c779c947bfef4e",
+    "url": "/oxford-hackathon/static/media/leftLightening.0cbd653d.png"
+  }
+]);
